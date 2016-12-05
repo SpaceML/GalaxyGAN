03a13ad98af4b33d28836cc6014f1948f01f0a04
Ubuntu <ubuntu@ip-172-31-19-171.ec2.internal>
2016-12-05 14:48:23 +0000

    2 modes

--- test.sh
+++ test.sh
@@ -37,6 +37,9 @@ while [ "$1" != "" ]; do
 	-output )		shift
 				result_dir=$1
 				;;
+	-mode )			shift
+				mode=$1
+				;;
         -h | --help )           usage
                                 exit
                                 ;;
@@ -53,8 +56,12 @@ matlabcommand="roou("$fwhm","$sig",'"$input_dir"','"$figure_dir"',1);quit"
 matlab -nosplash -nodesktop -r $matlabcommand
 
 cd pix2pix
-DATA_ROOT=../$base_dir/$name name=$fwhm"_"$sig which_direction=BtoA phase=test display=0 gpu=$user_gpu checkpoints_dir=../$model_dir results_dir=../$result_dir th test.lua
-
+if ["$mode" == full]; then
+	DATA_ROOT=../$base_dir/$name name=$fwhm"_"$sig which_direction=BtoA phase=test display=0 gpu=$user_gpu checkpoints_dir=../$model_dir results_dir=../$result_dir th test.lua
+else
+	cp ../$base_dir/$name/test_no_orginal/* ../$base_dir/$name/test/		
+	DATA_ROOT=../$base_dir/$name name=$fwhm"_"$sig which_direction=BtoA phase=test display=0 gpu=$user_gpu checkpoints_dir=../$model_dir results_dir=../$result_dir th test.lua
+fi
 cd ../$result_dir/$name/latest_net_G_test/
 mkdir images/deconv
 mogrify -path images/deconv -resize 256x256 -quality 100 ../../../$figure_dir/deconv/*.jpg
